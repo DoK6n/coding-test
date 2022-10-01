b20108297fa9af0de7c98c3d818b54da3022c3dc
Dok6n <ehrbs2018@gmail.com>
2022-10-01 17:30:30 +0900

    move directory

---
+++ hacker_rank/implementation/apple_and_orange.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * 과일이 나무에서 떨어지면 x축을 따라 원래 나무에서 d 단위 거리에 떨어집니다.
+ * d의 음수는 과일이 나무의 왼쪽으로 d단위 떨어졌음을 의미하고, d의 양수 값은 나무의 오른쪽으로 d단위 떨어진 것을 의미합니다.
+ *
+ * @param {number} s starting point of Sam's house location.
+ * @param {number} t ending location of Sam's house location.
+ * @param {number} a location of the Apple tree.
+ * @param {number} b location of the Orange tree.
+ * @param {number[]} apples distances at which each apple falls from the tree.
+ * @param {number[]} oranges distances at which each orange falls from the tree.
+ * @return {void}
+ */
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  // Write your code here
+}
+
+const n = 0;
+const m = 0;
+
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
+
+// correct output 1 1
