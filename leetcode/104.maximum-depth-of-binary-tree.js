2183dc60b6c482f3ac28d51f51b69bd4840ca738
Dok6n <ehrbs2018@gmail.com>
2022-10-21 03:45:29 +0900

    leetcode/dfs(E)

--- leetcode/104.maximum-depth-of-binary-tree.js
+++ leetcode/104.maximum-depth-of-binary-tree.js
@@ -21,27 +21,29 @@ function TreeNode(val, left, right) {
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function (root) {
-  let depth = 0;
+// const maxDepth = root => {
+//   let depth = 0;
 
-  const dfs = (node, lv) => {
-    // 자식 노드가 더 이상 없는 레벨까지 탐색한 경우
-    if (!node) {
-      // 트리의 마지막 노드의 레벨과 최근 탐색했던 마지막 노드의 레벨 중 최대값 할당
-      // lv - 1인 이유는 마지막 노드의 left와 right가 호출된 상태이기 때문에
-      depth = Math.max(lv - 1, depth);
-      return;
-    }
-    dfs(node.left, lv + 1); // 왼쪽 노드 탐색 -> 탐색시마다 2번째 인자에 레벨 1 증가
-    dfs(node.right, lv + 1);
-  };
+//   const dfs = (node, lv) => {
+//     // 자식 노드가 더 이상 없는 레벨까지 탐색한 경우
+//     if (!node) {
+//       // 트리의 마지막 노드의 레벨과 최근 탐색했던 마지막 노드의 레벨 중 최대값 할당
+//       // lv - 1인 이유는 마지막 노드의 left와 right가 호출된 상태이기 때문에
+//       depth = Math.max(lv - 1, depth);
+//       return;
+//     }
+//     dfs(node.left, lv + 1); // 왼쪽 노드 탐색 -> 탐색시마다 2번째 인자에 레벨 1 증가
+//     dfs(node.right, lv + 1);
+//   };
 
-  dfs(root, 1);
+//   dfs(root, 1);
 
-  return depth;
-};
+//   return depth;
+// };
 
+const maxDepth = root => (
+  root
+    ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+    : 0 
+);
 // @lc code=end
-/**
- * 깊이우선탐색
- */
---
+++ leetcode/111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=111 lang=javascript
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const minDepth = root => {
+  if (!root) return 0; // x/\x
+  if (!root.right) return minDepth(root.left) + 1; // o/\x
+  if (!root.left) return minDepth(root.right) + 1; // x/\o
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
+// @lc code=end
